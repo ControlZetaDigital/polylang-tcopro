2cd8fa3da558878d707f1d869bdf03db27a4979f
Deploy <deploy@vps-eb45f99e.vps.ovh.net>
2026-01-15 10:32:52 +0000

    Release 1.1.3: fix incorrect plugin header version

--- polylang-tcopro.php
+++ polylang-tcopro.php
@@ -3,7 +3,7 @@
  * Plugin Name:       Polylang for Tco Pro
  * Plugin URI:        https://github.com/ControlZetaDigital/polylang-tcopro
  * Description:       Integrates Polylang with Theme.co Pro theme headers, footers and layouts
- * Version:           1.1.1
+ * Version:           1.1.3
  * Author:            ControlZeta
  * Author URI:        https://controlzetadigital.com
  * Text Domain:       polylang-tcopro
