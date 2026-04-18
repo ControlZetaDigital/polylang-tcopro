99e1fed597197a755ed06c6dc03f88fa3ea11a2b
Deploy <deploy@vps-eb45f99e.vps.ovh.net>
2026-04-18 01:29:12 +0000

    Cleanup: remove boilerplate docblocks and add WPINC guard to view partials

--- admin/views/partials/widget-assignments.php
+++ admin/views/partials/widget-assignments.php
@@ -1,57 +1,50 @@
-<?php
-/**
- * Widget item list and language assignments
- *
- *
- * @link       https://github.com/ControlZetaDigital/polylang-tcopro
- * @since      1.1.0
- *
- * @package    polylang-tcopro
- * @subpackage polylang-tcopro/admin/views/partials
- */
-
-?>
+<?php defined( 'WPINC' ) || die; ?>
 
 <div class="<?php echo esc_attr( POLYLANG_TCOPRO_NAME ); ?>-widget-assignments">
 
-    <?php foreach($widget->items as $item) : ?>
+	<?php foreach ( $widget->items as $item ) : ?>
 
-        <?php $item_languages = $ptco->getItemLanguages($item->ID); ?>
+		<?php $item_languages = $ptco->getItemLanguages( $item->ID ); ?>
 
-        <div class="<?php echo esc_attr( POLYLANG_TCOPRO_NAME ); ?>-assignment">
+		<div class="<?php echo esc_attr( POLYLANG_TCOPRO_NAME ); ?>-assignment">
 
-            <div class="<?php echo esc_attr( POLYLANG_TCOPRO_NAME ); ?>-assignment-item">
+			<div class="<?php echo esc_attr( POLYLANG_TCOPRO_NAME ); ?>-assignment-item">
 
-                <h4><a target="_blank" href="<?php echo esc_url( home_url( "/cornerstone/edit/{$item->ID}" ) ); ?>"><span class="dashicons dashicons-edit"></span></a> <?php echo esc_html( $item->title ); ?> (Id: <?php echo absint( $item->ID ); ?>)</h4>
+				<h4>
+					<a target="_blank" href="<?php echo esc_url( home_url( "/cornerstone/edit/{$item->ID}" ) ); ?>">
+						<span class="dashicons dashicons-edit"></span>
+					</a>
+					<?php echo esc_html( $item->title ); ?> (Id: <?php echo absint( $item->ID ); ?>)
+				</h4>
 
-                <ul class="<?php echo esc_attr( POLYLANG_TCOPRO_NAME ); ?>-language-list">
+				<ul class="<?php echo esc_attr( POLYLANG_TCOPRO_NAME ); ?>-language-list">
 
-                <?php foreach($languages as $lang) : ?>
+					<?php foreach ( $languages as $lang ) : ?>
 
-                    <li>
+						<?php
+						$default  = ( $lang['slug'] === $ptco->defaultLanguage() ) ? ' default' : '';
+						$selected = ( $item_languages && in_array( $lang['slug'], $item_languages->list, true ) ) ? ' selected' : '';
+						?>
 
-                        <?php $default = ($lang["slug"] === $ptco->defaultLanguage()) ? ' default' : ''; ?>
+						<li>
+							<a class="<?php echo esc_attr( POLYLANG_TCOPRO_NAME ); ?>-flag<?php echo esc_attr( $default . $selected ); ?>"
+							   data-language="<?php echo esc_attr( $lang['slug'] ); ?>">
+								<img src="<?php echo esc_url( $lang['flag'] ); ?>" width="18" height="18" />
+							</a>
+						</li>
 
-                        <?php $selected = ($item_languages && in_array($lang["slug"], $item_languages->list, true)) ? ' selected' : ''; ?>
+					<?php endforeach; ?>
 
-                        <a class="<?php echo esc_attr( POLYLANG_TCOPRO_NAME ); ?>-flag<?php echo esc_attr( $default . $selected ); ?>" data-language="<?php echo esc_attr( $lang["slug"] ); ?>">
+				</ul>
 
-                            <img src="<?php echo esc_url( $lang["flag"] ); ?>" width="18" height="18" />
+			</div>
 
-                        </a>
+			<input type="hidden"
+			       name="item_<?php echo absint( $item->ID ); ?>_languages"
+			       <?php echo $item_languages ? 'value="' . esc_attr( $item_languages->value ) . '"' : ''; ?> />
 
-                    </li>
+		</div>
 
-                <?php endforeach; ?>
-
-                </ul>
-
-            </div>
-
-            <input type="hidden" name="item_<?php echo absint( $item->ID ); ?>_languages" <?php echo $item_languages ? 'value="' . esc_attr( $item_languages->value ) . '" ' : ''; ?>/>
-
-        </div>
-
-    <?php endforeach; ?>
+	<?php endforeach; ?>
 
 </div>
